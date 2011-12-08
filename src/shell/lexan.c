f0f1f90d1a4932307b0f0169bf48b5f7497fd58b
MOZGIII <mike-n@narod.ru>
2011-12-08 21:00:25 +0400

    Implemented tab autocompletion, a kind of :\

--- src/shell/lexan.c
+++ src/shell/lexan.c
@@ -3,6 +3,7 @@
 #include <conf.h>
 #include <kernel.h>
 #include <shell.h>
+#include <cmd.h>
 
 #define NOCHAR  0x100       /* Value that is not ASCII */
 #define ctoi(x) ((int)(unsigned char)(x))         /* put this in kernel.h */
@@ -23,7 +24,7 @@ char    *prompt;
     for (ch=shptr->shlastc; ch == ' ' || ch == '\t'
                          || ch == 0  ; ) /* skip white space and ctrl B */
         ch= shgetc(shptr, NULLSTR);
-    if ((toktype=ch)=='\n' || toktype==EOF) 
+    if ((toktype=ch)=='\n' || toktype==EOF)
         return(toktype);
     if (plevel=(toktype == '('? 1 : 0))
         ch=shgetc(shptr, NULLSTR);
@@ -62,6 +63,31 @@ char    *prompt;
     return(toktype);
 }
 
+// Does all the magic on autocompletion
+char* calculate_autocomplete(char * buff, int len) {
+  int com, matched_command;
+
+  // Find command in commands list
+  matched_command = -1;
+  for (com = 0 ; com < ncmds ; com++) {
+    if (strncmp(cmds[com].cmdnam, buff, len)==0) {
+      // Only one match allowed!
+      if(matched_command > -1)
+        return NULL;
+      // Save matched command index
+      matched_command = com;
+    }
+  }
+
+  // No matches found!
+  if(matched_command == -1)
+    return NULL;
+
+  // Matched command found!
+  // Shift it's name to len...
+  return cmds[matched_command].cmdnam + len;
+}
+
 /*------------------------------------------------------------------------
  *  shgetc  -  get a character from the shell's command input device
  *------------------------------------------------------------------------
@@ -71,6 +97,8 @@ char    *prompt;
  char           *prompt;
  {
     int     len;
+    int     i;
+    char    *ac;
 
     if (shptr->shnextc < shptr->shend)
         return(shptr->shlastc = ctoi(*shptr->shnextc++));
@@ -78,8 +106,32 @@ char    *prompt;
         xinu_write(STDOUT, prompt, xinu_strlen(prompt));
     if ((len=xinu_read(shptr->shcmddev, shptr->shbuf, SHBUFLEN)) <= 0)
         return(shptr->shlastc = EOF);
+
+    /* Fetch tabs */
+
+    if (xinu_strlen(shptr->shbuf) > 1 && shptr->shbuf[xinu_strlen(shptr->shbuf)-2] == '\t') {
+      // Lets see if we can autocomplete command...
+      ac = calculate_autocomplete(shptr->shbuf, len-2);
+      if (ac) {
+        // Very durty code here... but who cares...
+        // it's not even a serious task...
+
+        xinu_write(STDOUT, prompt, xinu_strlen(prompt));
+        len -= 2;
+        xinu_write(STDOUT, shptr->shbuf, len);
+        xinu_write(STDOUT, ac, xinu_strlen(ac));
+        xinu_write(STDOUT, " ", xinu_strlen(" "));
+        strcpy(shptr->shbuf + len, ac);
+        len += xinu_strlen(ac);
+        strcpy(shptr->shbuf + len, " ");
+        len += 1;
+      } // else do nothing!
+    }
+
+
     shptr->shbuf[len] = '\0';
     shptr->shnextc = shptr->shbuf;
     shptr->shend   = &shptr->shbuf[len];
+
     return(shptr->shlastc = ctoi(*shptr->shnextc++));
 }
